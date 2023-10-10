75dc2d8bad4011fb629ab9fd4b30a8039f125aa8
Ph.Adamczyk <ph.adamczyk@campus.lmu.de>
2023-10-10 15:32:43 +0200

    Added DatasetCollection and OfflineTrainer to F2MD

---
+++ machine-learning-server/MLDatasetCollection.py
@@ -0,0 +1,151 @@
+import os
+import numpy as np
+import pickle
+from tqdm import tqdm
+import sys
+
+
+def determineCurrentModel(model):
+    if model == 1:
+        return "SVM"
+    elif model == 2:
+        return "MLP_L1N25"
+    elif model == 3:
+        return "MLP_L3N25"
+    elif model == 4:
+        return "LSTM"
+    else:
+        raise ValueError("Not suitable model")
+
+
+def determineCurrentCheckVersion(checkVersion):
+    if checkVersion == 1:
+        return "Legacy"
+    elif checkVersion == 2:
+        return "Catch"
+    elif checkVersion == 3:
+        return "Experi"
+    else:
+        raise ValueError("Unknown Checks Version")
+
+
+def saveData(valuesData, targetData, partition, savePath, currentModel, checkVersion):
+    with open(
+        savePath
+        + "/valuesSave_"
+        + currentModel
+        + "_"
+        + checkVersion
+        + "_"
+        + str(partition)
+        + ".listpkl",
+        "wb",
+    ) as fp:
+        pickle.dump(valuesData, fp)
+    fp.close()
+    with open(
+        savePath
+        + "/targetSave_"
+        + currentModel
+        + "_"
+        + checkVersion
+        + "_"
+        + str(partition)
+        + ".listpkl",
+        "wb",
+    ) as ft:
+        pickle.dump(targetData, ft)
+    ft.close()
+
+
+def loadData(file_path, savePath):
+    with open(savePath + "/" + file_path, "rb") as f:
+        data = pickle.load(f)
+    f.close()
+    data = np.array(data)
+    return data
+
+
+def appendData(data, newDataPath, savePath):
+    with open(savePath + "/" + newDataPath, "rb") as f:
+        dataAppend = pickle.load(f)
+    f.close()
+    dataAppend = np.array(dataAppend)
+    retValues = np.append(data, dataAppend, axis=0)
+    return retValues
+
+
+def main():
+    partition_numbers = 10
+    # partitions = [(h+1)/partition_numbers for h in range(partition_numbers)]
+    partitions = [1.0]
+    model = int(sys.argv[1])
+    if model == 1:
+        partitions = [round(elem / 10, 2) for elem in partitions]
+    savePath = "/F2MD/machine-learning-server"
+    currentModel = determineCurrentModel(model)
+    checkVersion = determineCurrentCheckVersion(int(sys.argv[2]))
+    loadPath = (
+        savePath
+        + "/saveFile/data/"
+        + checkVersion
+        + "_Checks_Data/"
+        + currentModel
+    )
+    # loadPath = savePath + "/saveFile/" + currentModel
+    savePath = savePath + "/saveFile/concat_data"
+    print(savePath)
+    print(loadPath)
+    firstValue = True
+    firstTarget = True
+    dataFileNames = [
+        elem
+        for elem in os.listdir(loadPath)
+        if (elem[-8:] == ".listpkl" and not "together" in elem)
+    ]
+    valuesFileNames = [elem for elem in dataFileNames if "values" in elem]
+    targetFileNames = [elem for elem in dataFileNames if "target" in elem]
+    # print(len(targetFileNames))
+    valuesNamesLength = len(valuesFileNames)
+    partition_sizes = [
+        int(round(partitions[j] * valuesNamesLength, 0)) for j in range(len(partitions))
+    ]
+    # print(partition_sizes)
+    for i in tqdm(range(valuesNamesLength)):
+        valuesFileName = valuesFileNames[i]
+        targetFileName = targetFileNames[i]
+        if firstValue:
+            # print("Saving: " + valuesFileName)
+            valuesData = loadData(valuesFileName, loadPath)
+            firstValue = False
+        else:
+            valuesData = appendData(valuesData, valuesFileName, loadPath)
+        # print(valuesData.shape)
+        if firstTarget:
+            # print("Saving: " + targetFileName)
+            targetData = loadData(targetFileName, loadPath)
+            firstTarget = False
+        else:
+            targetData = appendData(targetData, targetFileName, loadPath)
+        # print(targetData.shape)
+        if model == 1:
+            rounded_partition = round((i + 1) / valuesNamesLength, 2)
+        else:
+            rounded_partition = round((i + 1) / valuesNamesLength, 1)
+        # print(rounded_partition)
+        if (i + 1) in partition_sizes:
+            saveData(
+                valuesData,
+                targetData,
+                rounded_partition,
+                savePath,
+                currentModel,
+                checkVersion,
+            )
+            partitions.pop(0)
+            partition_sizes.pop(0)
+            # print(partitions)
+
+
+if __name__ == "__main__":
+    main()
---
+++ machine-learning-server/MLOfflineTrainer.py
@@ -0,0 +1,104 @@
+if __name__ == "__main__":
+    print("Importing...")
+    from sklearn.svm import SVC
+    from sklearn.neural_network import MLPClassifier
+    from sklearn.preprocessing import MinMaxScaler
+    from keras.models import Sequential  
+    from keras.layers import Dense, LSTM
+    from keras.callbacks import ReduceLROnPlateau
+    from keras.utils import to_categorical
+    from MLDatasetCollection import determineCurrentCheckVersion
+    import numpy as np
+    import joblib
+    import pickle
+    import sys
+    model = int(sys.argv[1])
+    partition = sys.argv[3]
+    checkVersion = determineCurrentCheckVersion(int(sys.argv[2]))
+    extra = "_30_n_iter_100_relu_lbfgs"
+    print("Starting...")
+    savePath = "/F2MD/machine-learning-server/saveFile"
+    if model == 1:
+        print("Starting Data Loading; Partition: " + checkVersion + "_" + partition)
+        with open (savePath+'/concat_data/'+"valuesSave_SVM_" + checkVersion + "_" + partition + ".listpkl", 'rb') as fp:
+            X = pickle.load(fp)
+        X = np.array(X)
+        with open (savePath+'/concat_data/'+"targetSave_SVM_" + checkVersion + "_" + partition + ".listpkl", 'rb') as ft:
+            y = pickle.load(ft)
+        y = np.array(y)
+        print("Data loaded")
+        clf = SVC(gamma=0.001, C=100., verbose=1)
+        print("Starting machine learning: SVM")
+        clf.fit(X, y)
+        print("Machine Learning done; saving model")
+        joblib.dump(clf, savePath + '/clfs/clf_SVM_SINGLE_' + checkVersion + "_" + partition + ".pkl")
+    elif model == 2:
+        print("Starting Data Loading; Partition: " + checkVersion + "_" + partition)
+        with open (savePath+'/concat_data/'+"valuesSave_MLP_L1N25_" + checkVersion + "_" + partition + ".listpkl", 'rb') as fp:
+            X = pickle.load(fp)
+        X = np.array(X)
+        with open (savePath+'/concat_data/'+"targetSave_MLP_L1N25_" + checkVersion + "_" + partition + ".listpkl", 'rb') as ft:
+            y = pickle.load(ft)
+        y = np.array(y)
+        print("Data loaded")
+        clf = MLPClassifier(alpha=1e-5,hidden_layer_sizes=(25,))
+        print("Starting machine learning: MLP_L1N25")
+        clf.fit(X, y)
+        print("Machine Learning done; saving model")
+        joblib.dump(clf, savePath + '/clfs/clf_MLP_SINGLE_L1N25_' + checkVersion + "_" + partition + ".pkl")
+    elif model == 3:
+        print("Starting Data Loading; Partition: " + checkVersion + "_" + partition)
+        with open (savePath+'/concat_data/'+"valuesSave_MLP_L3N25_" + checkVersion + "_" + partition + ".listpkl", 'rb') as fp:
+            X = pickle.load(fp)
+        X = np.array(X)
+        with open (savePath+'/concat_data/'+"targetSave_MLP_L3N25_" + checkVersion + "_" + partition + ".listpkl", 'rb') as ft:
+            y = pickle.load(ft)
+        y = np.array(y)
+        print("Data loaded")
+        clf = MLPClassifier(alpha=1e-5,hidden_layer_sizes=(25,25,25), solver="lbfgs", activation="relu", random_state=1, n_iter_no_change=100, max_iter=100000)
+        print("Starting machine learning: MLP_L3N25")
+        clf.fit(X, y)
+        print("Machine Learning done; saving model")
+        joblib.dump(clf, savePath + '/clfs/clf_MLP_SINGLE_L3N25_' + checkVersion + "_" + partition + extra + ".pkl")
+    elif model == 4:
+        print("Starting Data Loading; Partition: " + checkVersion + "_" + partition)
+        with open (savePath+'/concat_data/'+"valuesSave_LSTM_" + checkVersion + "_" + partition + ".listpkl", 'rb') as fp:
+            X = pickle.load(fp)
+        X = np.array(X)
+        with open (savePath+'/concat_data/'+"targetSave_LSTM_" + checkVersion + "_" + partition + ".listpkl", 'rb') as ft:
+            y = pickle.load(ft)
+        y = np.array(y)
+        y = to_categorical(y)
+        print(X.shape)
+        print(y.shape)
+        print("Data loaded")
+        clf = Sequential()
+        clf.add(LSTM(128, return_sequences=True, input_shape=(len(X[0]), len(X[0][0]))))
+        clf.add(LSTM(128, return_sequences=True))
+        clf.add(LSTM(128, return_sequences=False))
+        clf.add(Dense(y.shape[1],activation='softmax'))  
+        clf.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['accuracy'])
+        reduce_lr = ReduceLROnPlateau(monitor='accuracy', factor=0.7,patience=4, min_lr=0.0005)
+        clf.fit(X, y,epochs=10, batch_size=64, callbacks=[reduce_lr])
+        print("Starting machine learning: LSTM")
+        clf.fit(X, y)
+        print("Machine Learning done; saving model")
+        joblib.dump(clf, savePath + '/clfs/clf_LSTM_RECURRENT_' + checkVersion + "_" + partition + ".pkl")
+    else:
+        raise ValueError("Not a suitable model selected")
+    if model == 2 or model == 3:
+        print("Classes: " + str(clf.classes_) + "\n")
+        print("Loss: " + str(round(clf.loss_,4)) + "\n")
+        for i in range(len(clf.coefs_)):
+            print("----------Coef " + str(i) + ":----------\n")
+            coef = clf.coefs_[i]
+            print(str(coef) + "\n")
+        # print("Best Loss: " + str(round(clf.best_loss_,4)) + "\n")
+        # rounded_losses = [round(loss,4) for loss in clf.loss_curve_]
+        # print("Loss Corve: " + str(rounded_losses) + "\n")
+        # print("Validation Scores: " + str(clf.validation_scores_) + "\n")
+        # print("Best Validation Score: " + str(round(clf.best_validation_score_,4)) + "\n")
+        print("N Features: " + str(clf.n_features_in_) + "\n")
+        print("T: " + str(clf.t_) + "\n")
+        print("N Iterations: " + str(clf.n_iter_) + "\n")
+        
