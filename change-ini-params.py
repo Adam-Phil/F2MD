464121fccf9c09eb7764e0c49e6ccb06d6e27891
Ph.Adamczyk <ph.adamczyk@campus.lmu.de>
2023-10-11 09:53:03 +0200

    Added the attacker density to the change of ini-params

--- change-ini-params.py
+++ change-ini-params.py
@@ -12,26 +12,39 @@ def map_number_to_scenario(number):
     else:
         raise ValueError("Unknow scenario")
 
-def substitute_first_by_int(content, number):
-    content = list(content)
-    for i in range(len(content)):
-        if content[i].isdigit():
-            number_char = str(number)[0]
-            content[i] = number_char
-            break
-    return "".join(content)
 
-def change_content_according_to_params(content,app,check):
-    content_list = content.split("checksVersionV1")
-    substituted_check = substitute_first_by_int(content_list[1],check)
-    content_list = [content_list[0], substituted_check]
-    new_content = content_list[0] + "checksVersionV1" + content_list[1]
-    content_list = new_content.split("appTypeV1")
-    substituted_app = substitute_first_by_int(content_list[1],app)
-    content_list = [content_list[0], substituted_app]
-    new_content = content_list[0] + "appTypeV1" + content_list[1]
+def substitute_first_by_param(content, param):
+    content = list(content)
+    param_plus_space = " = " + str(param) + " \n"
+    line_end = ""
+    for i in range(len(content)):
+        line_end = line_end + "".join(content[i])
+        if content[i] == "\n":
+            break
+    content = "".join(content)
+    content = content.replace(line_end, param_plus_space, 1)
+    # print(param)
+    # print(content)
+    return content
+
+
+def change_single_param(content, name, param):
+    content_list = content.split(name)
+    substitute = substitute_first_by_param(content_list[1], param)
+    new_content = content_list[0] + name
+    new_content = new_content + substitute
+    return new_content
+
+
+def change_content_according_to_params(content, app, check, attacker_density):
+    new_content = change_single_param(
+        content=content, name="checksVersionV1", param=check
+    )
+    new_content = change_single_param(content=new_content, name="appTypeV1", param=app)
+    new_content = change_single_param(content=new_content, name="LOCAL_ATTACKER_PROB", param=attacker_density)
     # print(new_content)
     return new_content
+
 
 if __name__ == "__main__":
     import os
@@ -45,6 +58,11 @@ if __name__ == "__main__":
     check = int(sys.argv[3])
     if not (check <= 2 and check >= 0):
         raise ValueError("Check type out of bounds")
+    attacker_density = float(sys.argv[4])
+    if attacker_density > 1:
+        attacker_density = attacker_density / 100
+    if not (attacker_density >= 0 and attacker_density <= 1):
+        raise ValueError("Attacker density out of bounds")
     scenario_string = map_number_to_scenario(scenario_int)
     scenario_path = network_path + scenario_string
     if not (os.path.exists(scenario_path) and os.path.isdir(scenario_path)):
@@ -57,7 +75,9 @@ if __name__ == "__main__":
             with open(ini_path, "r") as o_file:
                 content = o_file.read()
                 o_file.close()
-            new_content = change_content_according_to_params(content,app,check)
+            new_content = change_content_according_to_params(
+                content, app, check, attacker_density
+            )
             with open(ini_path, "w") as o_file:
                 o_file.write(new_content)
                 o_file.close()
